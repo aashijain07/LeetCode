30a82f8001201ec35294b77f94d8c07d5cb14d65
Aashi Jain <80680617+aashijain07@users.noreply.github.com>
2023-07-09 00:07:56 +0530

    Time: 215 ms (53.27%) | Memory: 63.4 MB (69.76%) - LeetSync

---
+++ put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {    
+       vector<int>v;
+       for(int i = 0;i<weights.size()-1;i++)v.push_back(weights[i]+weights[i+1]);
+       sort(v.begin(),v.end());
+       int i = 0,j = weights.size()-2;
+       long long ans = 0;
+       while(i<k-1){
+           ans+=(v[j]-v[i]);
+           i++,j--;
+       }
+       return ans;
+    }
+};
