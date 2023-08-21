d116ad8d049d57439761033cc7c01c38e11afb71
Aashi Jain <80680617+aashijain07@users.noreply.github.com>
2023-08-21 23:37:55 +0530

    Time: 51 ms (28.91%) | Memory: 91.7 MB (21.92%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
+        
+       int n = s.length();
+    
+    if (n < 2) {
+        return false;
+    }
+    
+    for (int l = 1; l <= n/2; ++l) {
+        if (n % l== 0) {
+            string sub_str = s.substr(0, l);
+            string reconstructed_str = "";
+            for (int j = 0; j < n/l; ++j) {
+                reconstructed_str += sub_str;
+            }
+            if (reconstructed_str == s) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+
+        
+    }
+};
+
+
+/*
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
 
         for(int i=s.size()-1; i>=0; i--){
             if(i==0) return false;
@@ -10,3 +41,4 @@ public:
         return false;
     }
 };
+*/
